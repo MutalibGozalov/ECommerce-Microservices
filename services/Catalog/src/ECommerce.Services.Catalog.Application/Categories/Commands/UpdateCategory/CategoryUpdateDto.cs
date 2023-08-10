ffb8a9c1de5e442bd8db8e41569b2bd5d5fe6b15
Mutalib SH. Gozalov <mutalibshg@code.local>
2023-08-10 19:07:39 +0400

    Unnecessery Dtos deleted from Catalog microservice

--- services/Catalog/src/ECommerce.Services.Catalog.Application/Categories/Commands/UpdateCategory/CategoryUpdateDto.cs
+++
@@ -1,7 +0,0 @@
-
-namespace ECommerce.Services.Catalog.Application.Categories.Commands.UpdateCategory;
-public class CategoryUpdateDto
-{
-    public string Name { get; set; } = null!;
-    public string? SubcategoryId { get; set; }
-}
--- services/Catalog/src/ECommerce.Services.Catalog.Application/ProductVariations/Commands/UpdateProductVariation/ProductVariationUpdateDto.cs
+++
@@ -1,9 +0,0 @@
-
-namespace ECommerce.Services.Catalog.Application.ProductVariations.Commands.UpdateProductVariation;
-public class ProductVariationUpdateDto
-{
-    public int SKU { get; set; }
-    public int Quantity { get; set; }
-    public decimal Price { get; set; }
-    public string[] Media { get; set; } = null!;
-}
