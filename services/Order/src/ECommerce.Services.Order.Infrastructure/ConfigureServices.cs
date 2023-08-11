ab48aeb4e0f6d533e6700968ac6f45bc9b4155c3
Mutalib SH. Gozalov <mutalibshg@code.local>
2023-08-11 18:46:04 +0400

    working on: Migration error

--- services/Order/src/ECommerce.Services.Order.Infrastructure/ConfigureServices.cs
+++ services/Order/src/ECommerce.Services.Order.Infrastructure/ConfigureServices.cs
@@ -6,11 +6,11 @@ public static class ConfigureServices
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddScoped<AuditableEntitySaveChangesInterceptor>();
-        services.AddDbContext<AppDbContext>(options =>
+        services.AddDbContext<IAppDbContext, AppDbContext>(options =>
             options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
             builder => builder.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)));
 
-        services.AddScoped<IAppDbContext>(provider => provider.GetRequiredService<AppDbContext>());
+        services.AddScoped(provider => provider.GetRequiredService<IAppDbContext>());
 
         services.AddScoped<AddDbContextInitializer>();
 
