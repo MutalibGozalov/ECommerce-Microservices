fdfc0e99dfe79c8dd37fc086584f2d58624bc463
Mutalib SH. Gozalov <mutalibshg@code.local>
2023-08-15 12:00:47 +0400

    GetAccessTokenByRefreshToken done

--- Frontends/ECommerce.Web/Services/IdentityService.cs
+++ Frontends/ECommerce.Web/Services/IdentityService.cs
@@ -103,12 +103,63 @@ public class IdentityService : IIdentityService
 
     }
 
-    public Task<TokenResponse> GetAccessTokenByRefreshToken()
+    public async Task<TokenResponse> GetAccessTokenByRefreshToken()
     {
-        throw new NotImplementedException();
+        var discovery = await _httpClient.GetDiscoveryDocumentAsync(new DiscoveryDocumentRequest
+        {
+            Address = _serviceApiSettings.BaseUri,
+            Policy = new DiscoveryPolicy { RequireHttps = false }
+        });
+
+        if (discovery.IsError)
+        {
+            throw discovery.Exception ?? new Exception("Discovery exception thrown DARLIN!");
+        }
+
+        var refreshToken = await _httpContextAccessor.HttpContext.GetTokenAsync(OpenIdConnectParameterNames.RefreshToken);
+
+        RefreshTokenRequest refreshTokenRequest = new()
+        {
+            ClientId = _clientSettings.WebClientForUser.ClientId,
+            ClientSecret = _clientSettings.WebClientForUser.ClientSecret,
+            RefreshToken = refreshToken,
+            Address = discovery.TokenEndpoint
+        };
+
+        var token = await _httpClient.RequestRefreshTokenAsync(refreshTokenRequest);
+        if (token.IsError is true)
+        {
+            return null;
+        }
+
+
+        var authenticationTokens = new List<AuthenticationToken>
+        {
+            new AuthenticationToken {
+                Name = OpenIdConnectParameterNames.AccessToken,
+                Value = token.AccessToken
+            },
+            new AuthenticationToken {
+                Name = OpenIdConnectParameterNames.RefreshToken,
+                Value = token.RefreshToken
+            },
+            new AuthenticationToken {
+                Name = OpenIdConnectParameterNames.ExpiresIn,
+                Value = DateTime.Now.AddSeconds(token.ExpiresIn).ToString("o", CultureInfo.InvariantCulture)
+            }
+        };
+
+        var authenticationResult = await _httpContextAccessor.HttpContext.AuthenticateAsync();
+        var properties = authenticationResult.Properties;
+
+        properties.StoreTokens(authenticationTokens);
+
+        await _httpContextAccessor.HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, authenticationResult.Principal, properties);
+
+        return token;
     }
 
-    public Task RevokeRefreshToken()
+    public async Task RevokeRefreshToken()
     {
         throw new NotImplementedException();
     }
