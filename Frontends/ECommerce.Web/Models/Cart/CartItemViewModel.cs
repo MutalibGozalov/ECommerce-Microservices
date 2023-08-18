3cfb9b3593e98fd145f9f90c63ea604c67510e0c
Mutalib Gozalov <mutalibshg@code.edu.az>
2023-08-18 12:46:05 +0400

    Cart and CartItems view models set up

---
+++ Frontends/ECommerce.Web/Models/Cart/CartItemViewModel.cs
@@ -0,0 +1,20 @@
+using System.Reflection.Metadata.Ecma335;
+
+namespace ECommerce.Web.Models.Cart;
+public class CartItemViewModel
+{
+    public string? ProductId { get; set; }
+    public string? Name { get; set; }
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+
+    private decimal? DiscountPrice { get; set; }
+    public decimal GetCurrentPrice
+    {
+        get => DiscountPrice is not null ? DiscountPrice.Value : Price;
+    }
+    public void AppliedDiscount(decimal discountPrice)
+    {
+        DiscountPrice = discountPrice;
+    }
+}
---
+++ Frontends/ECommerce.Web/Models/Cart/CartViewModel.cs
@@ -0,0 +1,38 @@
+namespace ECommerce.Web.Models.Cart;
+public class CartViewModel
+{
+    public string UserId { get; set; } = null!;
+    public string? DiscountCode { get; set; }
+    public int? DiscountRate { get; set; }
+    
+    private List<CartItemViewModel>? _cartItems;
+
+    public List<CartItemViewModel>? CartItems
+    {
+        get
+        {
+            if (HasDiscount)
+            {
+                _cartItems.ForEach(item =>
+                {
+                    var discountPrice = item.Price * ((decimal)DiscountRate.Value / 100);
+                    item.AppliedDiscount(Math.Round(item.Price - discountPrice, 2));
+                });
+            }
+            return _cartItems;
+        }
+        set
+        {
+            _cartItems = value;
+        }
+    }
+    public decimal TotalPrice
+    {
+        get => _cartItems.Sum(c => c.GetCurrentPrice * c.Quantity);
+    }
+
+    public bool HasDiscount
+    {
+        get => string.IsNullOrEmpty(DiscountCode) is false;
+    }
+}
