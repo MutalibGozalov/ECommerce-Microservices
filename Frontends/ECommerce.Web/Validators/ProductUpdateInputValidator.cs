2cd59d7ef0e953fa8cc6077636ae28da271a19af
Mütalib Gözelov <mutalib.gezalov.9848@mail.ru>
2023-08-18 21:07:05 +0400

    FluentValidation Rules added

---
+++ Frontends/ECommerce.Web/Validators/ProductUpdateInputValidator.cs
@@ -0,0 +1,27 @@
+using ECommerce.Web.Models.Catalog;
+using FluentValidation;
+
+namespace ECommerce.Web.Validators;
+public class ProductUpdateInputValidator : AbstractValidator<ProductUpdateInput>
+{
+    public ProductUpdateInputValidator()
+    {
+        RuleFor(p => p.Name)
+        .NotEmpty()
+        .WithMessage("Product Name can not be null DARLIN! ");
+
+        RuleFor(p => p.Description)
+        .NotEmpty()
+        .WithMessage("Description can not be null DARLIN!");
+
+        RuleFor(p => p.DisplayPrice)
+        .NotEmpty()
+        .WithMessage("Price can not be null DARLIN!")
+        .PrecisionScale(2, 6, true)
+        .WithMessage("Incorrect price, coreect form: $$$$.$$");
+
+        RuleFor(p => p.StoreId)
+        .NotEmpty()
+        .WithMessage("Store Id can not be null DARLIN!");
+    }
+}
