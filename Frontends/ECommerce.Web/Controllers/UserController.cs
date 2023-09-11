8032239344a4ad820d6a2fce521f37cb6aecc358
Mütalib Gözelov <mutalib.gezalov.9848@mail.ru>
2023-09-11 20:03:01 +0400

    StoreProducts partial view done

--- Frontends/ECommerce.Web/Controllers/UserController.cs
+++ Frontends/ECommerce.Web/Controllers/UserController.cs
@@ -9,11 +9,14 @@ public class UserController : Controller
 {
     private readonly IUserService _userService;
     private readonly IOrderService _orderService;
+    private readonly ICatalogService _catalogService;
 
-    public UserController(IUserService userService, IOrderService orderService)
+
+    public UserController(IUserService userService, IOrderService orderService, ICatalogService catalogService)
     {
         _userService = userService;
         _orderService = orderService;
+        _catalogService = catalogService;
     }
 
     public async Task<IActionResult> Index()
@@ -21,6 +24,7 @@ public class UserController : Controller
         var orders = await _orderService.GetOrders();
 
         ViewBag.orders = orders;
+        ViewBag.StoreProductList = await _catalogService.GetAllProductsAsync();
 
 
         return View("Account", await _userService.GetUser());
